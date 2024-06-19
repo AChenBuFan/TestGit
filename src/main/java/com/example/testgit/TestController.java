93196bb02c8d68f151fe7bc4fe6f0593e29af530
chenqf <chenqf@sgcctd.com>
2024-06-20 02:08:40 +0800

    feat(feature-01): 功能01-v1~v5

--- src/main/java/com/example/testgit/TestController.java
+++ src/main/java/com/example/testgit/TestController.java
@@ -7,4 +7,8 @@ package com.example.testgit;
  **/
 public class TestController {
     private static final String TEST_GIT = "wordGit";
+    private static final String TEST_GIT_1 = "wordGit1";
+    private static final String TEST_GIT_2 = "wordGit1";
+    private static final String TEST_GIT_3 = "wordGit1";
+    private static final String TEST_GIT_4 = "wordGit1";
 }
