322fbb19bbfed85a546bbae1573c8b4249482480
chenqf <chenqf@sgcctd.com>
2024-06-20 02:08:40 +0800

    feat(feature-01): 功能01

---
+++ src/main/java/com/example/testgit/TestController.java
@@ -0,0 +1,10 @@
+package com.example.testgit;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Author Chenqf
+ **/
+public class TestController {
+    private static final String TEST_GIT = "wordGit";
+}
